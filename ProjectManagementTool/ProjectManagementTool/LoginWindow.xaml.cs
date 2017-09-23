1e0f40179fe3c1cb7eee3e816cf9588e35c42baf
kogi1337 <alexkogler0@gmail.com>
2017-09-23 17:27:53 +0100

    added login, register, main and forgot pw windows

---
+++ ProjectManagementTool/ProjectManagementTool/LoginWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.VisualBasic;
+
+namespace ProjectManagementTool
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //TODO Check textboxes
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            Main main = new Main(txtUsername.Text, txtPassword.Password);
+            main.Show();
+        }
+
+        //Opens register window
+        private void RegisterLabel_Click(object sender, RoutedEventArgs e)
+        {
+            RegisterWindow register = new RegisterWindow();
+            register.Show();
+        }
+
+        //Opens forget pw window
+        private void ForgotPwLabel_Click(object sender, RoutedEventArgs e)
+        {
+            ForgotPasswordWindow forgot = new ForgotPasswordWindow();
+            forgot.Show(); 
+        }
+    }
+}
---
+++ ProjectManagementTool/ProjectManagementTool/RegisterWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProjectManagementTool
+{
+    /// <summary>
+    /// Interaction logic for RegisterWindow.xaml
+    /// </summary>
+    public partial class RegisterWindow : Window
+    {
+        public RegisterWindow()
+        {
+            InitializeComponent();
+        }
+
+        //This method just goes back to the login screen
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        //TODO Register user and check on textboxes
+        private void btnRegister_Click(object sender, RoutedEventArgs e)
+        {
+            
+            this.Close();
+        }
+    }
+}
