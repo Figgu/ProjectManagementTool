91125705ca9dba6634979fad61143b42bf7d3438
kogi1337 <alexkogler0@gmail.com>
2017-10-08 13:36:26 +0200

    guis for main and add project

--- ProjectManagementToolApp/app/src/main/java/com/projectmanagementtoolapp/activities/MainActivity.java
+++ ProjectManagementToolApp/app/src/main/java/com/projectmanagementtoolapp/activities/MainActivity.java
@@ -1,6 +1,8 @@
 package com.projectmanagementtoolapp.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -12,11 +14,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.projectmanagementtoolapp.R;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
+
+    private CoordinatorLayout layout;
+    private FloatingActionButton fab;
+    private ListView projectsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,15 +36,6 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -43,6 +44,17 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        setTitle("Your projects");
+        getAllViews();
+        addFAB();
+        addList();
+    }
+
+    private void getAllViews() {
+        fab = (FloatingActionButton) findViewById(R.id.fabMain);
+        layout = (CoordinatorLayout) findViewById(R.id.layoutMain);
+        projectsList = (ListView) findViewById(R.id.projectsList);
     }
 
     @Override
@@ -55,34 +67,12 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-
+        /*
         if (id == R.id.nav_camera) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
@@ -96,9 +86,29 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_send) {
 
         }
-
+        */
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void addFAB() {
+        fab.setOnClickListener(this);
+    }
+
+    private void addList() {
+        String[] mobileArray = {"Android","IPhone","WindowsMobile","Blackberry",
+                "WebOS","Ubuntu","Windows7","Max OS X"};
+
+        ArrayAdapter adapter = new ArrayAdapter<>(this, R.layout.listview, mobileArray);
+        projectsList.setAdapter(adapter);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == fab) {
+            Intent intent = new Intent(this, AddProjectActivity.class);
+            startActivity(intent);
+        }
     }
 }
