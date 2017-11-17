808f4289a0d7894f7f481b3f6ca7caf16175fc0d
kogi1337 <alexkogler0@gmail.com>
2017-11-17 09:11:51 +0100

    added picture property

--- ProjectManagementToolApp/app/src/main/java/com/projectmanagementtoolapp/pkgData/User.java
+++ ProjectManagementToolApp/app/src/main/java/com/projectmanagementtoolapp/pkgData/User.java
@@ -11,7 +11,7 @@ public class User {
     private String username;
     private String password;
     private String email;
-    //TODO blob
+    private byte[] profilePicture;
     private List<Project> projects;
 
     public User(int userID, String username, String password, String email) {
@@ -68,6 +68,14 @@ public class User {
 
     public void setProjects(List<Project> projects) {
         this.projects = projects;
+    }
+
+    public byte[] getProfilePicture() {
+        return profilePicture;
+    }
+
+    public void setProfilePicture(byte[] profilePicture) {
+        this.profilePicture = profilePicture;
     }
 
     @Override
